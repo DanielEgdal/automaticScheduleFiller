f799190dfa6dec6a987b8be785d7732fb7f7f18b
Daniel V. Egdal <daniel.v.egdal@gmail.com>
2023-10-15 17:48:49 +0200

    fix fm, and more than 2 days

--- dwschedule.py
+++ dwschedule.py
@@ -6,7 +6,7 @@ from openpyxl import load_workbook
 import os
 
 def getDaySplit(schedule):
-	daySplit=[] # see when the schedule devides in two days. It doesn't work for more than 2 days.
+	daySplit= [0] # Starting at 0, as this indicates the first event of the comp
 	for i in range(1,len(schedule)):
 		if schedule[i][1].day == schedule[i-1][1].day:
 			pass
@@ -21,11 +21,12 @@ def fillInCells(sch,i,val,event_hit,eventPro,eventcount,eventCut):
 		if event_hit[val[0]] == 1: # first round
 			sch.cell(row=i,column=10).value = eventcount[val[0]] # set competitors
 		else: # advancement conditions for the rest
-			if eventPro[val[0]][event_hit[val[0]]][0]: # Ranking based
-				sch.cell(row=i,column=10).value = eventPro[val[0]][event_hit[val[0]]][1]
-			else: # Percentage based
-				sch.cell(row=i,column=10).value = int((eventPro[val[0]][event_hit[val[0]]][1]/100)*eventcount[val[0]])
-		if val[0] != '3x3 MBLD':
+			if val[0] not in {'3x3 MBLD','3x3 FMC'}:
+				if eventPro[val[0]][event_hit[val[0]]][0]: # Ranking based
+					sch.cell(row=i,column=10).value = eventPro[val[0]][event_hit[val[0]]][1]
+				else: # Percentage based
+					sch.cell(row=i,column=10).value = int((eventPro[val[0]][event_hit[val[0]]][1]/100)*eventcount[val[0]])
+		if val[0] not in {'3x3 MBLD','3x3 FMC'}:
 			if eventCut[val[0]][event_hit[val[0]]] == 1: # Cumulative time limit
 				sch.cell(row=i,column=8).value = eventCut[val[0]][event_hit[val[0]]][1]
 			elif eventCut[val[0]][event_hit[val[0]]] == 2: # Cutoff
@@ -55,7 +56,7 @@ def wallinSchedule(data,stations):
 	for event in data["events"]:
 		for round in event['rounds']:
 			eventname = round['id'].split('-')
-			if eventname[0] != '333mbf':
+			if eventname[0] not in {'333mbf','333fm'}:
 				if round['timeLimit']['cumulativeRoundIds']:
 					eventCut[mapping[eventname[0]]][int(eventname[1][1:])] = (1,round['timeLimit']['centiseconds']*6000)
 				elif round['cutoff']:
@@ -88,9 +89,10 @@ def wallinSchedule(data,stations):
 	schedule.sort(key=lambda x:x[1]) # Sort based on start time
 
 	daySplit = getDaySplit(schedule)
+	print(daySplit)
 
 	script_dir = os.path.dirname(os.path.abspath(__file__))
-	filename = os.path.join(script_dir, 'dwscheduleMar2023.xlsx')
+	filename = os.path.join(script_dir, 'dwscheduleOct2023.xlsx')
 
 	wb = load_workbook(filename = filename) # open the schedule template
 
@@ -98,26 +100,24 @@ def wallinSchedule(data,stations):
 	sch = wb[sheet_names[0]] # The interesting tab
 	sch.cell(row=2,column=17,value=stations)
 
-	# start time
-	sch.cell(row=6,column=1,value=schedule[0][1].hour)
-	sch.cell(row=6,column=2).value = schedule[0][1].minute
-
-	i = 8 # row of first activity
 	event_hit = defaultdict(int)
 
-	if daySplit:
-		for val in schedule[:daySplit[0]]:
-			i = fillInCells(sch,i,val,event_hit,eventPro,eventcount,eventCut)
-		sch.cell(row=29,column=1,value=schedule[daySplit[0]][1].hour)
-		sch.cell(row=29,column=2).value = schedule[daySplit[0]][1].minute
-		i = 31 # jump to day 
-		for val in schedule[daySplit[0]:]:
-			i = fillInCells(sch,i,val,event_hit,eventPro,eventcount,eventCut)
-	else: # One day comp, again basically the same
-		for val in schedule:
-			i = fillInCells(sch,i,val,event_hit,eventPro,eventcount,eventCut)
+	differentDayInsertion(daySplit,schedule,sch,event_hit,eventPro,eventcount,eventCut)
 
 	# wb.save(f"dwschedule{id}.xlsx")
 	return wb
+
+def differentDayInsertion(daySplit,schedule,sheet,event_hit,eventPro,eventcount,eventCut):
+	startingPointDay = [8,31,54,80] # The first row for the day
+	for idx,day in enumerate(daySplit): 
+		i = startingPointDay[idx]
+		continue_to = len(schedule)
+		if idx+1 < len(daySplit):
+			continue_to = daySplit[idx+1]
+		sheet.cell(row=i-2,column=1,value=schedule[day][1].hour) # Starting time of the day
+		sheet.cell(row=i-2,column=2).value = schedule[day][1].minute
+		for val in schedule[daySplit[idx]:continue_to]:
+			i = fillInCells(sheet,i,val,event_hit,eventPro,eventcount,eventCut)
+
 
 
